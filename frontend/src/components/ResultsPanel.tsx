97dd0ad3302d0ec562a004096a51e366676b9de4
AryanNair-24 <aryan.nair@mail.utoronto.ca>
2026-03-12 23:57:45 -0400

    style search results

--- frontend/src/components/ResultsPanel.tsx
+++ frontend/src/components/ResultsPanel.tsx
@@ -8,15 +8,20 @@ interface ResultsPanelProps {
 
 function ResultsPanel({ result }: ResultsPanelProps) {
     return (
-        <div>
-            <h1>{result.title}</h1>
-            {result.poster 
-                ? <img src={`https://image.tmdb.org/t/p/w500${result.poster}`} />
-                : <p>No poster available</p>
-            }
-            {result.platforms.map((platform) => (
-                <p key={platform}>{platform}</p>
-            ))}
+        <div className="flex gap-4 mt-6 p-4 bg-gray-800 rounded-lg">
+            <div>
+                {result.poster 
+                ? <img className="w-32 rounded" src={`https://image.tmdb.org/t/p/w500${result.poster}`} />
+                : <p className='w-32'>No poster available</p>
+                }
+            </div>
+            
+            <div>
+                <h1 className='text-xl font-bold'>{result.title}</h1>
+                {result.platforms.map((platform) => (
+                    <p className='text-sm text-gray-400' key={platform}>{platform}</p>
+                ))}
+            </div>
         </div>
     )
 }
