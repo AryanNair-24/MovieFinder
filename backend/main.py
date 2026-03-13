ed6b5aaa13f0cb0365c4414cb833b2cc9dafe699
AryanNair-24 <aryan.nair@mail.utoronto.ca>
2026-03-13 14:39:36 -0400

    fix frontend URL

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://movie-finder-liart-gamma.vercel.app/"],
+    allow_origins=["http://localhost:5173", "https://movie-finder-git-main-aryannair-24s-projects.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
